43192f92f1d83f69665c81fdb78ef82ed2184837
Jonathan Riggins <jonathanriggins6924@gmail.com>
2018-02-02 13:49:03 -0800

    Autonomous chooser ready for testing

--- src/org/usfirst/frc/team3219/robot/subsystems/AutoManager.java
+++ src/org/usfirst/frc/team3219/robot/subsystems/AutoManager.java
@@ -2,37 +2,74 @@ package org.usfirst.frc.team3219.robot.subsystems;
 
 import org.usfirst.frc.team3219.robot.Robot;
 
-public class AutoManager {
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class AutoManager extends Command{
 	// Class Constants
 	static final boolean RIGHT_SIDE = false;
 	static final boolean LEFT_SIDE = true;
 	
+	@Override
+	protected void initialize() {
+		this.setTimeout(15);
+		autoCommandDecide();
+	}
+	
 	public void autoCommandDecide() {
 		String strategy = Robot.strategyChooser.getSelected();
 		String startPosition = Robot.startPositionChooser.getSelected();
+		String gameData;
+		gameData = DriverStation.getInstance().getGameSpecificMessage();
+		Robot.autoManager.setSideOwnership(gameData);
+		
 		if(startPosition == "Left") {
 			if(strategy == "AutoLine") {
-				Robot.autonomousCommand.start();
+				Robot.autoForward.start();
 			} else if(strategy == "AutoLine & Switch") {
-				
+				if(Robot.allianceSwitch == RIGHT_SIDE) {
+					Robot.autoForward.start();
+				} else if(Robot.allianceSwitch == LEFT_SIDE) {
+					Robot.autoForward.start();
+				}
 			} else if(strategy == "AutoLine & Scale") {
-				
+				if(Robot.scale == RIGHT_SIDE) {
+					
+				} else if(Robot.scale == LEFT_SIDE) {
+					
+				}
 			}
 		} else if(startPosition == "Center") {
 			if(strategy == "AutoLine") {
-				
+
 			} else if(strategy == "AutoLine & Switch") {
-				
+				if(Robot.allianceSwitch == RIGHT_SIDE) {
+
+				} else if(Robot.allianceSwitch == LEFT_SIDE) {
+
+				}
 			} else if(strategy == "AutoLine & Scale") {
-				
+				if(Robot.scale == RIGHT_SIDE) {
+					
+				} else if(Robot.scale == LEFT_SIDE) {
+					
+				}
 			}
 		} else if(startPosition == "Right") {
 			if(strategy == "AutoLine") {
-				
+				Robot.autoForward.start();
 			} else if(strategy == "AutoLine & Switch") {
-				
+				if(Robot.allianceSwitch == RIGHT_SIDE) {
+					Robot.autoForward.start();
+				} else if(Robot.allianceSwitch == LEFT_SIDE) {
+					Robot.autoForward.start();
+				}
 			} else if(strategy == "AutoLine & Scale") {
-				
+				if(Robot.scale == RIGHT_SIDE) {
+					
+				} else if(Robot.scale == LEFT_SIDE) {
+					
+				}
 			}
 		}
 	}
@@ -59,5 +96,11 @@ public class AutoManager {
 		} else {
 			Robot.opposingSwitch = LEFT_SIDE;
 		}
+	}
+
+	@Override
+	protected boolean isFinished() {
+		boolean timedOut = this.isTimedOut();
+		return timedOut;
 	}
 }
